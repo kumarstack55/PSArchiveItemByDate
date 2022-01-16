14627453b6d481ca6f62f1b71a1a43839f0d29cc
kumarstack55 <kumarstack55@gmail.com>
2022-01-16 23:59:33 +0900

    Initial commit

---
+++ PSArchiveItemByDate/PSArchiveItemByDate.ps1
@@ -0,0 +1,10 @@
+﻿[CmdletBinding(SupportsShouldProcess)]
+Param([String[]][Parameter(Mandatory,ValueFromRemainingArguments)]$Item)
+
+Set-StrictMode -Version Latest
+
+Import-Module -Name $PSCommandPath.Replace('.ps1', '.psd1')
+
+$Item | ForEach-Object {
+    Move-ItemToArchiveDirectoryByDateInName $_ | Out-Null
+}
